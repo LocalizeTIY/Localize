2751cca1574ab7de5dc883bc8c3103a3f03f3041
Hyona Kim <tekkieninja@gmail.com>
2015-11-30 10:20:04 -0500

    Initial Commit, basic scaffold

---
+++ js/app-core/config.js
@@ -0,0 +1,21 @@
+let config = function($stateProvider, $urlRouterProvider) {
+  
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('root', {
+      abstract: true,
+      templateUrl: 'templates/app-layout/layout.tpl.html'
+    })
+    .state('root.home', {
+      url: '/',
+      controller: 'HomeController as vm',
+      templateUrl: 'templates/app-layout/home.tpl.html'
+    });
+  
+
+};
+
+config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+export default config;
