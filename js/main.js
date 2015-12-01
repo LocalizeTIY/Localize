27c68926eef7e895ed6c25ae971a8f5a7a47b99e
Hyona Kim <tekkieninja@gmail.com>
2015-12-01 10:11:16 -0500

    updated foundation to 6

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 import angular from 'angular';
 
+//Import jQuery & Foundation
+import 'foundation';
+import $ from 'jquery';
+
+//Starting up foundation
+$(document).foundation();
+
 import './app-core/index';
 import './app-layout/index';
 
 angular
   .module('app', ['app.core', 'app.layout'])
+  .run(function ($rootScope) {
+    //$viewContentLoaded comes from Ui Router
+    $rootScope.on('$viewContentLoaded', function (event, data) {
+      $(document).foundation();
+    });
+  })
 ;
