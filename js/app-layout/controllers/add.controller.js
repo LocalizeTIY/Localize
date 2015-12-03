a657e0ec24d71983222bed982dd42bf5e4ee90cf
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2015-12-03 09:14:49 -0500

    STAR RATING ADDED TO THE ADD PAGE NEED TO GRAB VALUE AND SEND TO DBS..pending

--- js/app-layout/controllers/add.controller.js
+++ js/app-layout/controllers/add.controller.js
@@ -1,10 +1,26 @@
-let AddController = function() {
+let AddController = function($scope) {
   
   let vm = this;
+  $scope.rate = 5;
+  $scope.max = 10;
+  $scope.isReadonly = false;
+
+  $scope.hoveringOver = function(value) {
+    $scope.overStar = value;
+    $scope.percent = 100 * (value / $scope.max);
+  };
+
+  $scope.ratingStates = [
+    {stateOn: 'fa-check-circle', stateOff: 'fa-check-circle-o'},
+    {stateOn: 'fa-star', stateOff: 'fa-start-o'},
+    {stateOn: 'fa-heart', stateOff: 'fa-ban'},
+    {stateOn: 'fa-heart'},
+    {stateOff: 'fa-power-off'}
+  ];
   
 
 };
 
-AddController.$inject = [];
+AddController.$inject = ['$scope'];
 
 export default AddController;
