25e8cabcd9effa82282b7382a48f3eb1dd79f0c0
Shalmali Padgaokar <shalmalidalvi@hotmail.com>
2015-12-08 15:55:51 -0500

    ADDED UNIQ LIST TO HOME PAGE.

--- js/app-layout/controllers/home.controller.js
+++ js/app-layout/controllers/home.controller.js
@@ -1,9 +1,12 @@
+import _ from 'underscore';
 let HomeController = function(SearchService) {
   
   let vm = this;
  
   vm.search = search;
+  vm.getGoData = getGoData;
   vm.results = [];
+  vm.getTagData = getTagData;
 
   function search (data) {
     SearchService.search(data).then( (res) => {
@@ -11,7 +14,38 @@ let HomeController = function(SearchService) {
     });
   } 
 
+  // function specific(data){
+  //   SearchService.specific(data).then((res)=>{
+  //     vm.results =res.data.results;
+  //     console.log(res);
+  //     //console.log(vm.results);
+  //   });
+  // }
+
+  function getTagData(){
+
+    SearchService.getTagData().then((res)=>{
+      vm.tags=[];
+      console.log(res.data.results);
+      let pluckedTags = _.pluck(res.data.results, 'tags');
+      console.log('plucked', pluckedTags);
+      vm.tags = _.uniq(pluckedTags);
+      console.log('uniqed', vm.tags);
+    });
+  }
+  getTagData();
 };
+
+function getGoData(){
+  SearchService.getGoData().then((res)=>{
+    vm.go=[];
+    let pluckedGo = _.pluck(res.data.results, 'name');
+    console.log('plucked', pluckedGo);
+    vm.go = _.uniq(pluckedGo);
+    console.log('uniqed', vm.go);
+
+  });
+}
 
 HomeController.$inject = ['SearchService'];
 
--- js/app-social/services/search.service.js
+++ js/app-social/services/search.service.js
@@ -3,9 +3,42 @@ let SearchService = function(PARSE, $http) {
   let eventURL = PARSE.URL + 'classes/events';
 
   this.search = search;
+  this.specific = specific;
+  this.getTagData = getTagData;
+  this.getGoData = getGoData;
 
   function search (objectId) {
     return $http.get(eventURL, PARSE.CONFIG);
+  }
+
+  function specific(data){
+    console.log('from the specific function on service',data);
+
+
+    return $http({
+      url: eventURL, 
+      method : "GET",
+      params : { where:  data },
+      headers: PARSE.CONFIG.headers
+    });
+  }
+
+  function getTagData(){
+    console.log('hello');
+    return $http({
+      url : eventURL,
+      method : 'GET',
+      headers : PARSE.CONFIG.headers
+    });
+  }
+
+  function getGoData(){
+    console.log('hello');
+    return $http({
+      url : eventURL,
+      method : 'GET',
+      headers : PARSE.CONFIG.headers
+    });
   }
 
 };
